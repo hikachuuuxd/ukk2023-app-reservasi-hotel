5a6373d6b91596faf5864393646827f7dd5c6a6d
hikachuuuxd <hikarimizu1111@gmail.com>
2023-03-01 17:07:18 +0700

    memperbaiki tampilan login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -24,8 +24,8 @@ switch($operator){
 $_SESSION['answer'] = $answer;
 ?>
 
-    <div class="row justify-content-center">
-        <div class="col-md-4 p-2">
+    <div class="row justify-content-center ">
+        <div class="col-md-6 p-2">
 
         @if(session()->has('success') )
         <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -51,14 +51,17 @@ $_SESSION['answer'] = $answer;
 
      
         
-        <main class="form-signin w-100 m-auto">
+        <main class="form-signin w-100 m-auto   p-5">
   <form action="/login" method="post">
     @csrf
-    <h1 class="h3 mb-3 fw-normal text-center">LOGIN</h1>
+    <h4 class=" mb-4  fw-normal text-center text-dark " style="letter-spacing: 3px">LOGIN
+    </h4>
+    <hr class="w-50 d-none mx-auto mb-5">
+   
 
-    <div class="form-floating">
-      <input type="email" name="email" class="form-control mb-2 @error('email')is-invalid  @enderror " id="email" placeholder="name@example.com" autofocus required >
-      <label for="email">Email address</label>
+    <div class="form-floating mt-5">
+      <input type="email" name="email" class="form-control mb-2 @error('email')is-invalid  @enderror  rounded-0 border-0 border-bottom  bg-transparent border-dark text-dark" id="email" placeholder="name@example.com" autofocus required >
+      <label for="email" class="text-dark">Email address</label>
 
       @error('email')
       <div class="invalid-feedback">
@@ -66,21 +69,21 @@ $_SESSION['answer'] = $answer;
       </div>
       @enderror
     </div>
-    <div class="form-floating">
-      <input type="password" name="password" class="form-control mb-2" id="password" placeholder="Password" required>
-      <label for="password">Password</label>
+    <div class="form-floating mt-5">
+      <input type="password" name="password" class="form-control mb-2 rounded-0 border-0 border-bottom  bg-transparent border-dark text-dark" id="password" placeholder="Password" required>
+      <label for="password"class="text-dark" >Password</label>
     </div>
 
-      <div class="row">
+      <div class="row mt-5 mb-5">
         <div class="col">
-          <div class=" d-block w-100 h-100 border text-center ">
-            <p class="mt-3"><?php echo $first_num . " " . $operator . " " . $second_num ?></p>
+          <div class=" d-block w-100 h-100 border text-center rounded-0 border-0 ">
+            <p class="mt-3 fw-semibold fs-3 text-dark"><?php echo $first_num . " " . $operator . " " . $second_num ?></p>
           </div>
         </div>
         <div class="col">
           <div class="form-floating">
-            <input type="number" name="answer" id="answer" class="form-control mb-2" id="cap" placeholder="captcha" >
-            <label for="captcha">Captcha</label>
+            <input type="number" name="answer" id="answer" class="form-control mb-2 rounded-0 border-0 border-bottom  bg-transparent border-dark  text-dark" id="cap" placeholder="captcha" >
+            <label for="captcha" class="text-dark">Captcha</label>
           </div>
         </div>
       </div>
@@ -88,8 +91,8 @@ $_SESSION['answer'] = $answer;
    
 
 
-    <button class="w-100 btn btn-lg btn-dark mt-2" type="submit">Sign in</button>
-    <small class="d-block text-center mt-3">Not register? <a href="/register">Register Now! </a></small>
+    <button class="w-100 d-block ms-auto  btn btn-lg  rounded-0  border-dark button-login" style="margin-top:100px"type="submit">Masuk</button>
+    <small class="d-block text-end mt-3 text-dark">Not register? <a href="/register" class="text-decoration-none text-black">Register Now!  </a></small>
   </form>
 </main>
         </div>
